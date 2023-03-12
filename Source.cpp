cb78981e2b76551bfcab96d280026ebb7fc27189
Vladyslav <77577351+vladikOm@users.noreply.github.com>
2023-03-12 15:30:50 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,113 @@
+
+#include <iostream>
+#include<vector>            //Для  class vector
+ #include"Unique_ptr.h"     //Для мого власного Unique_ptr
+#include<memory>            //Для  unique_ptr
+
+
+
+ using std::cout;
+ using std::cin ;
+ using std::endl;
+
+class myClass;
+void  Test1();
+void  Test2();
+void  Test3();
+void  Test4();
+void  Test5();
+
+
+
+
+int main()
+{
+    cout << "Let's go test" << endl << endl;
+
+    Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+
+    return 0;
+}
+
+
+
+template<typename T>
+void MyDeleter(T* ptr) 
+{
+    cout << "i'm delete pointer\n";
+    delete ptr;
+}
+
+
+template<typename T>
+struct MyDel 
+{ 
+    void operator() (T* ptr )
+       {
+        cout << "i'm delete pointer\n";
+        delete ptr; 
+       }
+}; 
+
+
+class myClass
+{
+
+public:
+    int age = 10;
+    myClass(int temp) : age( temp )
+    {cout << " myClass() \n";};
+
+    ~myClass() 
+    { cout << " ~myClass() \n"; };
+
+    void Show() 
+    { cout << " Show() \n"; }
+};
+
+void Test1()
+{   cout << "\n test #1 \n" << endl;
+    
+    Unique_ptr<int> ptr;
+    cout << endl;
+    Unique_ptr<float> pt1(nullptr);
+}
+
+void Test2()
+{
+    cout << "\n test #2\n " << endl;
+    Unique_ptr<myClass> ptr { new myClass (50) };
+    cout << "End test" << endl;
+}
+
+void Test3()
+{
+    cout << "\n test #3\n"  << endl;
+    Unique_ptr<int>ptr1{ new int };   cout << endl;
+    Unique_ptr<int>ptr2{ std::move (ptr1 )};    //Unique_ptr<int>ptr2{ ptr1 }; такий варіант не працює ptr1(lvalue)
+    cout << "End test " << endl;
+}
+  
+void Test4()
+{
+    cout << "\n test #4\n "   << endl;
+    Unique_ptr<int, void (*)(int*) > ptr2{ new int(1) , MyDeleter };
+    cout << "End test  " << endl;
+}
+
+void Test5()
+{
+    cout << "\n test #5\n " << endl;
+    MyDel<int> Dl;
+
+    cout << endl;
+    Unique_ptr<int, MyDel<int>>  ptr1(   new  int (20), Dl  );
+    cout << "End test " << endl;
+}
+  
+
+
---
+++ Unique_ptr.h
@@ -0,0 +1,143 @@
+
+ 
+#pragma once
+#define _NODISCARD [[nodiscard]] //Атрибут
+
+#include <iostream>
+#include <memory>
+
+template<class T>
+struct deleter
+{
+    constexpr deleter() noexcept = default;
+
+    deleter(const deleter<T> &) noexcept  { }
+
+
+    void operator()(T* ptr) const noexcept
+    {
+        if (ptr)
+            std::cout << "I'm delete ptr" << std::endl;
+        delete ptr;
+
+    }
+ 
+};
+
+template<class T, class D = deleter<T>>
+class Unique_ptr
+{
+private:
+    T* m_pointer;
+    D m_deleter;
+
+public:
+     
+    constexpr Unique_ptr() noexcept :  m_pointer(nullptr)                      //Коструктор за замовчуванням    
+    {
+        std::cout << "Unique_ptr()\n";
+    }
+    constexpr  Unique_ptr(nullptr_t) noexcept : m_pointer(nullptr) 
+    {
+         std::cout << "Unique_ptr(nullptr_t)\n";
+    }
+    
+    explicit Unique_ptr(T* ptr) noexcept : m_pointer(ptr)          //Коструктор(ініціалізований явно) який приймає один параметр
+      
+    {
+       std:: cout << "explicit Unique_ptr(T* ptr = nullptr)\n";
+    }
+
+  
+    //// * Коструктора копії  unique_ptr не існує тому що тільки 1 вказівник вказує на об'єкт
+  
+    Unique_ptr(const Unique_ptr& obj) = delete;            // DELETED COPY CONSTRUCTOR
+
+    Unique_ptr& operator=(const Unique_ptr& obj) = delete; // DELETED COPY ASSIGNMENT OPERATOR
+
+   
+   
+    
+    Unique_ptr(T* ptr, const D&del ) noexcept :
+        m_pointer(ptr), m_deleter(del)                   
+    {
+        std::cout << "Unique_ptr(T* ptr, const D& del)\n";
+    }
+
+   Unique_ptr(T* ptr, D&& del) noexcept :                  //Тут ми передаємовказівник і  вказівник на функцію власну для видалення ()
+        m_pointer(ptr), m_deleter(std::move(del))
+    {
+        std::cout << " Unique_ptr(T* ptr, D&& del)\n";
+    }
+   template<class U>
+    Unique_ptr(Unique_ptr<U>&& other) noexcept :              //Конструктор переміщення передаємо посилання на об'єкт 
+        m_pointer(other.release()), m_deleter(std::move(other.m_deleter)) 
+    {
+        std::cout << "Unique_ptr(Unique_ptr&& other)\n";
+    }
+
+     
+
+    ~Unique_ptr() noexcept
+    {
+        m_deleter(m_pointer);
+    }
+
+   Unique_ptr& operator= (nullptr_t) noexcept
+    {
+        reset();
+        return *this;
+    }
+
+    Unique_ptr& operator=(Unique_ptr&& other) noexcept
+    {
+        reset(other.release());
+        m_deleter = std::move(other.m_deleter);
+        return *this;
+    }
+
+    
+    _NODISCARD T* get() const noexcept
+    {
+        return m_pointer;
+    }
+
+    _NODISCARD D& get_deleter() noexcept
+    {
+        return m_deleter;
+    }
+
+    _NODISCARD const D& get_deleter() const noexcept
+    {
+        return m_deleter;
+    }
+
+    _NODISCARD  explicit operator bool() const noexcept
+    {
+        return m_pointer != nullptr;
+    }
+
+    _NODISCARD T& operator*() const noexcept
+    {
+        return *m_pointer;
+    }
+
+    _NODISCARD T* operator->() const noexcept
+    {
+        return m_pointer;
+    }
+
+    T* release() noexcept
+    {
+        T* ptr = m_pointer;
+        m_pointer = nullptr;
+        return ptr;
+    }
+
+    void reset(T* ptr = nullptr) noexcept
+    {
+        T* tmp = m_pointer;
+        m_pointer = ptr;
+        m_deleter(tmp);
+    }
+};
